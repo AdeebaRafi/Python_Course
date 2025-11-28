bb19e8154358b4212eef85f4ed4722d190d56168
AdeebaRafi <143268086+AdeebaRafi@users.noreply.github.com>
2025-11-29 00:21:20 +0500

    update

--- patterns.py
+++ patterns.py
@@ -77,5 +77,61 @@ print(c)
 # Example result
 # [6, 12, 18, 4, 8]
 
-n = [3, 6, 9, 2, 4]
-for a in n:
+list = [3, 6, 9, 2, 4]
+new_list = []
+for a in list:
+    new_list.append(a*2)
+print(new_list)
+
+# Given a string
+# s = "hello world"
+# Count how many times the letter 'l' appears.
+
+s = "hello world"
+cont=0
+for a in s:
+    if a == 'l':
+        cont+=1
+print(cont)
+
+# Given a string
+# s = "python"
+# Print the string in reverse.
+
+s = "python"
+for i in range(len(s) -1, -1, -1):
+    print(s[i])
+
+s = "adeeba"
+rev= ""
+for m in range(len(s) -1,-1,-1):
+    rev = rev + s[m]
+print(rev)
+
+s = "ehsan"
+rev = ""
+l= len(s)-1
+while l>=0:
+    rev = rev + s[l]
+    l-=1
+print(rev)
+
+s = "nomi"
+rev = s[::-1]
+print(rev)
+
+# Given a string
+# s = "open ai"
+# Remove all spaces and print the result
+s = " open ai "
+no_space = s.replace(" ","")
+print(no_space)
+
+# Given a string
+# s = "banana"
+# Find the first index where 'n' appears.
+s = "banana"
+for a in range(len(s)):
+    if s[a] == 'n':
+        print(a)
+        break
