a027bf42bfa0306fd7d51b517e72990419af28dd
AdeebaRafi <143268086+AdeebaRafi@users.noreply.github.com>
2026-01-01 22:46:59 +0500

    update

--- prac.py
+++ prac.py
@@ -9,8 +9,30 @@
 # else:
 #     print(y)
 
-q= int(input())
-if q%2==0:
-    print("Even Number")
-else:
-    print("Odd Number")
+# q= int(input())
+# if q%2==0:
+#     print("Even Number")
+# else:
+#     print("Odd Number")
+
+# Take three numbers.
+# Print the biggest number.
+
+# x = int(input())
+# y = int(input())
+# z = int(input())
+#
+# if x > y and x > z:
+#     print(x)
+# elif y > x and y > z:
+#     print(y)
+# else:
+#     print(z)
+
+
+# Take one number.
+# Print all numbers from 1 to that number
+
+p = int(input())
+for i in range(1, p+1):
+    print(i)
