995db7e879888beffd6a1b8ed29e823e3426ccc6
AdeebaRafi <143268086+AdeebaRafi@users.noreply.github.com>
2026-01-01 21:41:18 +0500

    update

--- prac.py
+++ prac.py
@@ -1,3 +1,16 @@
-a = int(input())
-square = a * a
-print(square)
+# a = int(input())
+# square = a * a
+# print(square)
+
+# x = int(input())
+# y = int(input())
+# if x> y:
+#     print(x)
+# else:
+#     print(y)
+
+q= int(input())
+if q%2==0:
+    print("Even Number")
+else:
+    print("Odd Number")
