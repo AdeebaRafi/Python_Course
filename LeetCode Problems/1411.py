417f3dcd6919092d8e28c61c858924bc144e241e
AdeebaRafi <143268086+AdeebaRafi@users.noreply.github.com>
2026-01-03 18:42:43 +0500

    update

---
+++ LeetCode Problems/1411.py
@@ -0,0 +1,6 @@
+class Solution:
+    def numOfWays(self, n: int) -> int:
+        MOD = 10**9 + 7
+        A = B = 6
+            A, B = (2*A + 2*B) % MOD, (2*A + 3*B) % MOD
+        return (A + B) % MOD
