b6d749eee68cddc9c5fd51a31f35635039acb802
AdeebaRafi <143268086+AdeebaRafi@users.noreply.github.com>
2026-01-08 16:26:16 +0500

    update

--- prac.py
+++ prac.py
@@ -178,16 +178,29 @@ print(max_profit)
 
 #  [1, 2, 3, 4, 5] Reverse an array
 
-arr = [1, 2, 3, 4, 5]
-left = 0
-right = len(arr)-1
-while left < right:
-    arr[left], arr[right] = arr[right], arr[left]
-
-    left +=1
-    right -=1
-print(arr)
+# arr = [1, 2, 3, 4, 5]
+# left = 0
+# right = len(arr)-1
+# while left < right:
+#     arr[left], arr[right] = arr[right], arr[left]
+#
+#     left +=1
+#     right -=1
+# print(arr)
 
 # hello
-str= {'hello'}
+s = "hello"
+lst = list(s)
+
+left = 0
+right = len(lst) - 1
+
+while left < right:
+    lst[left], lst[right] = lst[right], lst[left]
+    left += 1
+    right -= 1
+
+result = "".join(lst)
+print(result)
+
 
