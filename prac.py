478c8baacdfd5d7624dcc4012fb13bcaf32c7afc
AdeebaRafi <143268086+AdeebaRafi@users.noreply.github.com>
2026-01-10 22:46:17 +0500

    update

--- prac.py
+++ prac.py
@@ -204,7 +204,7 @@ result = "".join(lst)
 print(result)
 
 
-s = "racecar"
+s = "python"
 
 left = 0
 right = len(s) - 1
@@ -219,3 +219,18 @@ while left < right:
     right -= 1
 
 print(is_palindrome)
+
+nums = [0, 1, 0, 3, 12]
+
+pos = 0
+
+for i in range(len(nums)):
+    if nums[i] != 0:
+        nums[pos] = nums[i]
+        pos += 1
+
+while pos < len(nums):
+    nums[pos] = 0
+    pos += 1
+
+print(nums)
