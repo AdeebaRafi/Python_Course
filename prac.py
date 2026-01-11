46a5af234327e955e98cbf6f325f5d2b89d25d97
AdeebaRafi <143268086+AdeebaRafi@users.noreply.github.com>
2026-01-11 20:42:14 +0500

    update

--- prac.py
+++ prac.py
@@ -234,3 +234,26 @@ while pos < len(nums):
     pos += 1
 
 print(nums)
+
+
+nums = [2, 1, 5, 1, 3, 2]
+k = 3
+
+window_sum = sum(nums[:k])
+max_sum = window_sum
+
+for i in range(k, len(nums)):
+    window_sum = window_sum + nums[i] - nums[i - k]
+    if window_sum > max_sum:
+        max_sum = window_sum
+
+print(max_sum)
+
+
+nums = [1, 3, 2, 6, -1, 4, 1, 8, 2]
+k = 2
+window_sum = sum(nums[:k])
+max_sum = window_sum
+for i in range
+4,5, 8,5,, 3, 5, 9,3
+9
