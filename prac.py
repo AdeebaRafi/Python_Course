adf91e526c7c378e8a49791516b300bfe6595543
AdeebaRafi <143268086+AdeebaRafi@users.noreply.github.com>
2026-02-02 23:06:02 +0500

    update

--- prac.py
+++ prac.py
@@ -344,3 +344,13 @@ for e in nums:
         s += e
 
 print("Sum of even numbers:", s)
+nums = input("Enter numbers with space: ")
+nums = nums.split()
+nums = [int(x) for x in nums]
+
+s = 0
+for e in nums:
+    if e % 2 == 0:
+        s += e
+
+print("Sum of even numbers:", s)
