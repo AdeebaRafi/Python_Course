31e6ad2a47d0329c1ee458e636abf88125c4b695
AdeebaRafi <143268086+AdeebaRafi@users.noreply.github.com>
2026-02-10 23:59:17 +0500

    update

--- practice.py
+++ practice.py
@@ -2,3 +2,10 @@ for  i in range(1, 4):
     print(i)
 for  i in range(15, 20):
     print(i)
+
+# Take one number.
+# Print all numbers from 1 to that number
+
+# p = int(input())
+# for i in range(1, p+1):
+#     print(i)
