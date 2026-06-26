46c2bfa38461cd28f819ee8e73d63617909a8d97
AdeebaRafi <143268086+AdeebaRafi@users.noreply.github.com>
2026-06-26 16:43:52 +0500

    update

---
+++ June Practice 2026/1.py
@@ -0,0 +1,22 @@
+# Problem 1: Even or Odd
+# x = int(input("Enter a number"))
+# if x%2==0:
+#     print("Even number")
+# else:
+#     print("Odd number")
+
+# Problem 2: Largest Number
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number"))
+# if a>b:
+#     print("Largest number is", a)
+# else:
+#     print("Largest number is", b)
+
+#
+# Problem 3: Sum of Numbers
+total = 0
+n = int(input("Enter number: "))
+for i in range(1,n+1):
+    total = i + total
+print(total)
