7c846a7fb192dfc4ec4b83bb3e5a416fd7c0a527
AdeebaRafi <143268086+AdeebaRafi@users.noreply.github.com>
2026-06-26 17:17:01 +0500

    update

--- June Practice 2026/1.py
+++ June Practice 2026/1.py
@@ -15,8 +15,23 @@
 
 #
 # Problem 3: Sum of Numbers
-total = 0
-n = int(input("Enter number: "))
-for i in range(1,n+1):
-    total = i + total
-print(total)
+# total = 0
+# n = int(input("Enter number: "))
+# for i in range(1,n+1):
+#     total = i + total
+# print(total)
+
+
+# Problem 4: Count Vowels
+count = 0
+vowel = ["a", "e", "i", "o", "u"]
+word = input("Enter a word: ")
+word = word.lower()
+for letter in word:
+    if letter in vowel:
+        count += 1
+print(count)
+
+# Problem 5: Multiplication Table
+
+
