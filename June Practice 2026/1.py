ec4ba865df253ca455808e5dbe59c72c0b747c49
AdeebaRafi <143268086+AdeebaRafi@users.noreply.github.com>
2026-06-28 16:35:56 +0500

    update

--- June Practice 2026/1.py
+++ June Practice 2026/1.py
@@ -35,11 +35,50 @@
 # print(count)
 
 # Problem 5: Multiplication Table
-total = 1
-n = int(input("Enter number: "))
-for i in range(1,11):
-    total = i * n
-    print(f"{n} x {i} = {total}")
+# total = 1
+# n = int(input("Enter number: "))
+# for i in range(1,11):
+#     total = i * n
+#     print(f"{n} x {i} = {total}")
 
 
 # Problem 6: Positive, Negative, or Zero
+
+# number = int(input("Enter a number: "))
+# if number>0:
+#     print("Positive number")
+# elif number<0:
+#         print("Negative number")
+# else:
+#     print("Zero")
+
+# Problem 7: Sum of Even Numbers
+# add = 0
+# sum = int(input("Enter a number: "))
+# for i in range(1, sum+1):
+#     if i%2 == 0:
+#         add = i + add
+# print(add)
+
+# Problem 8: Reverse a String
+# reverse = input("Enter a String: ")
+# reversed_text = ""
+# for char in reverse:
+#     reversed_text = char + reversed_text  # Adds to the front
+# print(reversed_text)
+
+# reverse = input("Enter a String: ")
+# reverse_text = reverse[::-1]
+# print(reverse_text)
+
+# Problem 9: Count Digits
+num = int(input("Enter a number: "))
+temp = num
+count = 0
+if num == 0:
+    count = 1
+else:
+    while temp >0:
+        count +=1
+        temp = temp // 10
+print(count)
